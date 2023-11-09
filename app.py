988fd46cf4b0580c167396a990e4047527629913
hmmi <hmmi@kakao.com>
2023-11-10 00:04:25 +0900

    홈 라우트 함수 변경

--- app.py
+++ app.py
@@ -4,9 +4,9 @@ app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():  # put application's code here
+def home():
     return render_template('index.html')
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
