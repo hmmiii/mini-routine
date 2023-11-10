6db13e5061b54dea36d190f395b61d37b0d66e42
hmmi <hmmi@kakao.com>
2023-11-10 14:19:51 +0900

    header, footer include 기본 세팅

--- app.py
+++ app.py
@@ -5,7 +5,10 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    context = {
+        'title': '미니루틴'
+    }
+    return render_template('index.html', data=context)
 
 
 if __name__ == '__main__':
