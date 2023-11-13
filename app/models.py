ca09c25586a768e4359a42238afe10e8261a4036
home <viostarryy@gmail.com>
2023-11-13 13:27:40 +0900

    model.py 테이블생성

--- app/models.py
+++ app/models.py
@@ -7,11 +7,18 @@ db_path = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'database.db'
 db_uri = f'sqlite:///{db_path}'
 
 class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(80), unique=True, nullable=False)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    pw = db.Column(db.String(120), nullable=False)
+    id = db.Column(db.String(100), primary_key=True)
+    username = db.Column(db.String(100), nullable=False)
+    email = db.Column(db.String(100), unique=True, nullable=False)
+    pw = db.Column(db.String(100), nullable=False)
 
     def __repr__(self):
-        return f'<User {self.username}>'
+        return f'<User {self.id}>'
+
+
+class Routine(db.Model):
+    rNum = db.Column(db.Integer, primary_key=True, index=True)
+    id = db.Column(db.ForeignKey('user.id'), nullable=False)
+    check = db.Column(db.Integer, nullable=False)
+    content = db.Column(db.Text, nullable=False)
 
