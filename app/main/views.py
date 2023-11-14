40641057c46927161879c0d76c1439beedd87fa3
hmmi <hmmi@kakao.com>
2023-11-14 15:28:21 +0900

    조회기능, 체크 기능, 등록 버튼 추가

--- app/main/views.py
+++ app/main/views.py
@@ -1,11 +1,26 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
+from app.models import Routine, db
 
 # 프로젝트 루트 디렉토리 경로
 main_bp = Blueprint('main', __name__, template_folder='../../templates')
 
+# 검사할 수 있는 로그인 아이디가 존재하지 않아 우선 루틴만 가지고 옴.
 @main_bp.route('/')
 def home():
+    id = 'testid'
     context = {
-        'title': '미니 루틴'
+        'title': '미니 루틴',
+        'routines' : Routine.query.filter_by(id=id).all(),
     }
     return render_template('index.html', data=context)
+
+@main_bp.route('/check', methods=['POST'])
+def check():
+    data = request.get_json()
+    print(data)
+    for i in data:
+        data = Routine.query.filter_by(rNum=i['rNum']).first()
+        data.check = int(i['check'])
+        db.session.add(data)
+        db.session.commit()
+    return 'OK'
