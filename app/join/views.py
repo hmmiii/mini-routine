1f7d640086db119adcb01fc340a12f374d499377
js1171 <bhd1171@naver.com>
2023-11-14 14:37:59 +0900

    회원가입 input 받아서 db에 있는지 확인

--- app/join/views.py
+++ app/join/views.py
@@ -3,11 +3,11 @@ import sqlite3
 
 join_bp = Blueprint('join', __name__, template_folder='../../templates')
 
-conn = sqlite3.connect('app\database.db')
-cursor = conn.cursor()
-query = "SELECT * FROM user"
-cursor.execute(query)
-rows = cursor.fetchall()
+# cona = sqlite3.connect('app\database.db')
+# cursora = cona.cursor()
+# querya = "SELECT * FROM user"
+# cursora.execute(querya)
+# rowsa = cursora.fetchall()
 
 @join_bp.route('/join', methods=['POST','GET'])
 def join() :
@@ -16,20 +16,38 @@ def join() :
     email = ""
     pw = ""
     conpw = ""
+    bid = ""
 
-    for row in rows :
-        print(row)
+    conn = sqlite3.connect('app\database.db')
+    cursor = conn.cursor()
 
+    query = "SELECT id FROM user"
+    cursor.execute(query)
+    rowa = cursor.fetchall()
+
+    # input POST 자료 받아옴
     if request.method == 'POST' :
         id = request.form['iid']
         name = request.form['iname']
         email = request.form['iemail']
         pw = request.form['ipw']
         conpw = request.form['iconpw']
-        
-    if pw==conpw :
-        print(id, name, email, pw, conpw)
+
+        select_id = "SELECT * FROM user WHERE id=?"
+        cursor.execute(select_id, (id,))
+        rows = cursor.fetchall()
+        for row in rows :
+            if row[0] is not None :
+                bid = row[0]
+                print(row)
+            else :
+                if pw==conpw :
+                    print(id, name, email, pw, conpw)
+                else:
+                    print("비밀번호와 비밀번호 확인란이 일치하지 않습니다!")
+
+
     
+    context = {"rowa" : rowa}
 
-
-    return render_template('join.html')
+    return render_template('join.html', data = context)
